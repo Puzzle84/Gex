9e35573bb8d29ab7c3b973f5b0241b26f9fb7b75
Paul Theunis <suluap@gmail.com>
2014-10-24 10:03:09 -0700

    implemented platform macros

--- include/system/gex_platform.h
+++ include/system/gex_platform.h
@@ -2,4 +2,47 @@
 #define GEX_PLATFORM_H
 
 
+//
+// NOTE: 
+//
+// Define pre-processor macros for DLL Export, import and 
+// system macros
+//
+
+#ifdef _WIN32
+	#define GEX_SYS_WINDOWS
+	//defined to disable <windows.h> from defining the min and max macros
+	#ifndef NOMINMAX
+	#define NOMINMAX
+	#endif
+
+#elif defined(__LINUX__)
+#define GEX_SYS_LINUX
+#elif defined (__APPLE__)
+#define GEX_SYS_MACOS
+#endif
+
+#ifndef GEX_STATIC_BUILD
+	#ifdef GEX_SYS_WINDOWS
+		#ifdef GEX_NONCLIENT_BUILD
+			#define GEX_API __dclspec(dllexport)
+		#else
+			#define GEX_API __dclspec(dllimport)
+		#endif
+
+		// Visual C++ compiler warning C4251 disable
+		#ifdef _MSC_VER
+			#pragma warning(disable : 4251)
+		#endif
+	#else
+		#ifdef __GNUC__ >= 4
+			#define GEX_API __attribute__ ((__visibility__("default")))
+		#else
+			#define GEX_API GEX_API
+		#endif
+	#endif
+#else
+	#define GEX_API GEX_API
+#endif
+
 #endif
